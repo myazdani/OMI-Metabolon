ee9a9ca632388e18d29ce5bc07e1d464ede76de8
Mehrdad Yazdani <myazdani@gmail.com>
2016-01-19 12:30:16 -0800

    color code top 25 metabolites for each comparison group

--- scripts/EXSC-08-15RD_from_adam/5-t-tests.R
+++ scripts/EXSC-08-15RD_from_adam/5-t-tests.R
@@ -82,10 +82,14 @@ pre.HC.ttest = merge(ttest.results, biochem.keys, by.x = "variable", by.y = "BIO
 
 library(ggplot2)
 library(plotly)
-ggplot(pre.HC.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL)) + geom_text() + ggtitle("HC vs Pre on raw log scaled data") -> p
+
+ggplot(pre.HC.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL)) + geom_text() + ggtitle("HC vs Pre on z-scaled data") -> p
 
 #ggplotly(p, filename = "metabolon/HC-pre-raw-log", fileopt = "overwrite")
-plotly_POST(p, filename = "metabolon/HC-pre-raw-log", fileopt = "overwrite")
+#plotly_POST(p, filename = "metabolon/HC-pre-z-scaled", fileopt = "overwrite")
+
+pre.HC.ttest.top = subset(pre.HC.ttest, (estimate > .15 | estimate < -.15) & p.value < .05)
+write.table(pre.HC.ttest.top, file = "./processedData/ECSC-08-15-from-Adam/low-p-high-est-HC-pre.tsv", sep = "\t",row.names = FALSE, quote = FALSE)
 
 #########################################################
 #
@@ -103,9 +107,11 @@ df.post.HC %>%
 
 post.HC.ttest = merge(ttest.results, biochem.keys, by.x = "variable", by.y = "BIOCHEM.name", all.x = TRUE)
 
-ggplot(post.HC.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL)) + geom_text() + ggtitle("HC vs Post on raw log scaled data") -> p
-plotly_POST(p, filename = "metabolon/HC-post-raw-log", fileopt = "overwrite")
+ggplot(post.HC.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL)) + geom_text() + ggtitle("HC vs Post on z-caled data") -> p
+#plotly_POST(p, filename = "metabolon/HC-post-z-scaled", fileopt = "overwrite")
 
+post.HC.ttest.top = subset(post.HC.ttest, (estimate > .15 | estimate < -.15) & p.value < .05)
+write.table(post.HC.ttest.top, file = "./processedData/ECSC-08-15-from-Adam/low-p-high-est-HC-post.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
 
 #########################################################
 #
@@ -123,5 +129,59 @@ df.pre.post %>%
 
 pre.post.ttest = merge(ttest.results, biochem.keys, by.x = "variable", by.y = "BIOCHEM.name", all.x = TRUE)
 
-ggplot(pre.post.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL)) + geom_text() + ggtitle("Pre vs Post on raw log scaled data") -> p
+ggplot(pre.post.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL)) + geom_text() + ggtitle("Pre vs Post on z-scaled data") -> p
+#plotly_POST(p, filename = "metabolon/pre-post-z-scaled", fileopt = "overwrite")
+
+pre.post.ttest.top = subset(pre.post.ttest, (estimate > .15 | estimate < -.15) & p.value < .05)
+write.table(pre.post.ttest.top, file = "./processedData/ECSC-08-15-from-Adam/low-p-high-est-pre-post.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
+
+library(xlsx)
+#write.xlsx(pre.HC.ttest.top, file = "./processedData/ECSC-08-15-from-Adam/top-metabolites-volcano.xlsx", sheetName="HC-vs-Pre",col.names=TRUE, append=FALSE, showNA=TRUE, row.names = FALSE)
+#write.xlsx(post.HC.ttest.top, file = "./processedData/ECSC-08-15-from-Adam/top-metabolites-volcano.xlsx", sheetName="HC-vs-Post",col.names=TRUE, append=TRUE, showNA=TRUE, row.names = FALSE)
+#write.xlsx(pre.post.ttest.top, file = "./processedData/ECSC-08-15-from-Adam/top-metabolites-volcano.xlsx", sheetName="Pre-vs-Post",col.names=TRUE, append=TRUE, showNA=TRUE, row.names = FALSE)
+
+
+#########################################################
+#
+# update plots to include top 25 metabolites for each cohort
+#
+#########################################################
+
+
+pre.HC.ttest.top$mag = abs(pre.HC.ttest.top$estimate) -log10(pre.HC.ttest.top$p.value)
+pre.HC.top.25.chems = pre.HC.ttest.top$variable[order(pre.HC.ttest.top$mag, decreasing = TRUE)[c(1:25)]]
+
+post.HC.ttest.top$mag = abs(post.HC.ttest.top$estimate) -log10(post.HC.ttest.top$p.value)
+post.HC.top.25.chems = post.HC.ttest.top$variable[order(post.HC.ttest.top$mag, decreasing = TRUE)[c(1:25)]]
+
+pre.post.ttest.top$mag = abs(pre.post.ttest.top$estimate) -log10(pre.post.ttest.top$p.value)
+pre.post.top.25.chems = pre.post.ttest.top$variable[order(pre.post.ttest.top$mag, decreasing = TRUE)[c(1:25)]]
+
+label.metabolites = function(df){
+  df$pre.hc = "Not.Sig"
+  df$pre.hc[which(df$variable %in% pre.HC.top.25.chems)] = "Pre.HC"
+  df$post.HC = "Not.Sig"
+  df$post.HC[which(df$variable %in% post.HC.top.25.chems)] = "Post.HC"
+  df$pre.post = "Not.Sig"
+  df$pre.post[which(df$variable %in% pre.post.top.25.chems)] = "Pre.Post"
+  df = melt(df, measure.vars = c("pre.hc", "post.HC", "pre.post"))
+  names(df)
+  return(df)
+}
+
+pre.HC.ttest = label.metabolites(pre.HC.ttest)
+post.HC.ttest = label.metabolites(post.HC.ttest)
+pre.post.ttest = label.metabolites(pre.post.ttest)
+
+ggplot(pre.HC.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL, colour = value)) + 
+  geom_text() + scale_colour_manual(values=c("gray", "red", "green", "blue")) + ggtitle("HC vs Pre on raw log scaled data")-> p
+plotly_POST(p, filename = "metabolon/HC-pre-raw-log", fileopt = "overwrite")
+
+ggplot(post.HC.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL, colour = value)) + 
+  geom_text() + scale_colour_manual(values=c("gray", "red", "green", "blue")) + ggtitle("HC vs Post on raw log scaled data")-> p
+plotly_POST(p, filename = "metabolon/HC-post-raw-log", fileopt = "overwrite")
+
+
+ggplot(pre.post.ttest, aes(x = estimate, y = -log10(p.value), label = BIOCHEMICAL, colour = value)) + 
+  geom_text() + scale_colour_manual(values=c("gray", "red", "green", "blue")) + ggtitle("Pre vs Post on raw log scaled data")-> p
 plotly_POST(p, filename = "metabolon/pre-post-raw-log", fileopt = "overwrite")
